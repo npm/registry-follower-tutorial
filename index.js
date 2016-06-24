31ddd355dd776afa0b22916eff8a41adb07bfe65
Ashley Williams <ashley666ashley@gmail.com>
2016-06-24 16:51:53 -0400

    add include_docs and log the change'swq doc object

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const ChangesStream = require('changes-stream');
 const db = 'https://replicate.npmjs.com';
 
 var changes = new ChangesStream({
-  db: db
+  db: db,
+  include_docs: true
 });
 
 changes.on('data', function(change) {
-  console.log(change);
+  console.log(change.doc);
 });
